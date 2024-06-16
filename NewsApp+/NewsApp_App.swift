dd85ac53df40544a4da3a564ed2cb9dcbe730b70
Vako <valeri.mekhashishvili97@gmail.com>
2024-06-16 06:18:40 +0400

    Initial Commit

---
+++ NewsApp+/NewsApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_App.swift
+//  NewsApp+
+//
+//  Created by valeri mekhashishvili on 16.06.24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
